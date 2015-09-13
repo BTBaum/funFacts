bf24ad0ca4c60b268d95e9a2344ae13cf363097d
Brandon Baum <railsdev14@gmail.com>
2015-09-13 19:42:12 -0400

    Basic app with model and simple controller

---
+++ Facts/Scratchpad.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Swift recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+someVariable = "anotherString"
+
+// var fruitsArray: [String] = ["apples"]
+var fruitsArray = ["apples"]
+fruitsArray.append("bananas")
+
+fruitsArray
+
+// Optionals
+
+var optionalString: String? = "hello"
+optionalString!
+
+// Random Number Generator
+
+var randomNumber = Int(arc4random_uniform(10))
+
+
+
+
+
+
